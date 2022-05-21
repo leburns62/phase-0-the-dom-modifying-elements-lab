3236ad3229157205ca81b8ff9d513826b4a8d8ee
Levi Hutchinson Burns <leburns62@gmail.com>
2022-05-21 17:27:20 -0400

    commit tree f228a8302f286d9bad22a6845943fc92f8c5192a

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.body.removeChild(main)
+
+const newHeader = document.createElement("h1");
+newHeader.id = ("victory");
+
+newHeader.innerHTML = "Levi is the champion";
+document.body.append(newHeader);
